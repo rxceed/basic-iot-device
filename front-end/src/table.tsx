d0021225f9d47804f819f0f2983840366f296f15
rxceed <m.risang25@gmail.com>
2025-06-30 06:28:58 +0700

    feat: streamlined with back end

--- front-end/src/table.tsx
+++ front-end/src/table.tsx
@@ -14,50 +14,48 @@ ChartJS.register(LineElement, PointElement, LinearScale, CategoryScale, Tooltip,
 
 type JsonData = Record<string, string | number | boolean>;
 
+const API_BASE_URI: string = import.meta.env.VITE_API_BASE_URI as string;
+
 const RealTimeTable: React.FC = () => {
   const [data, setData] = useState<JsonData>({
     time: "",
-    Temperature: 0,
-    Humidity: 0,
-    Water: 0,
+    temperature: 0,
+    humidity: 0,
+    intensity: 0,
   });
 
   const [temperatureHistory, setTemperatureHistory] = useState<number[]>([]);
   const [humidityHistory, setHumidityHistory] = useState<number[]>([]);
+  const [intensityHistory, setIntensityHistory] = useState<string[]>([]);
   const [labels, setLabels] = useState<string[]>([]);
+  const intensityLabels = ["Tidak Hujan", "Hujan Gerimis", "Hujan Deras"];
 
-  useEffect(() => {
+  useEffect(()=>{
     const interval = setInterval(() => {
-      const now = new Date();
-      const time = now.toLocaleTimeString();
-
-      const temp = Math.floor(Math.random() * 100);
-      const humid = Math.floor(Math.random() * 100);
-
-      const newData: JsonData = {
-        time,
-        Temperature: temp,
-        Humidity: humid,
-        Water:
-          Math.random() > 0.66
-            ? "Tidak Hujan"
-            : Math.random() > 0.5
-            ? "Gerimis"
-            : "Lebat",
-      };
-
-      setData(newData);
-
-      // Add to history (keep max 5 entries)
-      setTemperatureHistory((prev) => [...prev.slice(-9), temp]);
-      setHumidityHistory((prev) => [...prev.slice(-9), humid]);
-      setLabels((prev) => [...prev.slice(-9), time]);
-    }, 1000);
-
+      fetch(`${API_BASE_URI}/api/sensor_reading`, {
+      mode: "cors",
+      method: "GET"}).then().
+      then((tableData)=>tableData.json())
+      .then((data)=>{
+        const latestDataIndex = data.length - 1
+        const time = new Date(data[latestDataIndex].timestamp).toLocaleString();
+        const newData: JsonData = {
+          time: time,
+          temperature: data[latestDataIndex].temperature.toFixed(1),
+          humidity: data[latestDataIndex].humidity,
+          intensity: data[latestDataIndex].rain_intensity
+        }
+        setData(newData);
+        setTemperatureHistory((prev) => [...prev.slice(-9), data[latestDataIndex].temperature]);
+        setHumidityHistory((prev) => [...prev.slice(-9), data[latestDataIndex].humidity.toFixed(1)]);
+        setIntensityHistory((prev) => [...prev.slice(-9), data[latestDataIndex].rain_intensity]);
+        setLabels((prev) => [...prev.slice(-9), time]);
+      });
+    }, 5000);
     return () => clearInterval(interval);
-  }, []);
+  }, [])
 
-  const chartData = {
+  const chartDataTemHum = {
     labels: labels,
     datasets: [
       {
@@ -77,7 +75,7 @@ const RealTimeTable: React.FC = () => {
     ],
   };
 
-  const chartOptions = {
+  const chartOptionsTemHum = {
     responsive: true,
     plugins: {
       legend: {
@@ -90,6 +88,41 @@ const RealTimeTable: React.FC = () => {
         min: 0,
         max: 100,
       },
+    },
+  };
+
+  const mapIntensity = (value: any, index: any, array: any) => {
+    if(value === "Tidak Hujan") return 0;
+    else if(value === "Hujan Gerimis") return 1;
+    else if(value === "Hujan Deras") return 2;
+  }
+
+  const chartDataIntensity = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Rain Intensity",
+        data: intensityHistory.map(mapIntensity),
+        borderColor: "red",
+        backgroundColor: "rgba(255,0,0,0.1)",
+        fill: true,
+      },
+    ],
+  };
+
+  const chartOptionsIntensity = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: "top" as const,
+      },
+      scales: {
+          y: {
+            min: 0,
+            max: intensityLabels.length-1
+          }
+        }
     },
   };
 
@@ -123,13 +156,25 @@ const RealTimeTable: React.FC = () => {
           ))}
         </tbody>
       </table>
-
-      <div style={{ 
-        width: "90%", 
-        maxWidth: "600px", 
-        background: "rgba(255,255,255,0.7)", 
-        borderRadius: "16px" }}>
-        <Line data={chartData} options={chartOptions} />
+      <div style={{ display: 'flex', gap: '2rem', justifyContent: 'center' }}>
+        <div style={{ 
+          width: "45%",
+          height: "90%",
+          minWidth: "600px",
+          minHeight: "300px",
+          background: "rgba(255,255,255,0.7)", 
+          borderRadius: "16px" }}>
+          <Line data={chartDataTemHum} options={chartOptionsTemHum} />
+        </div>
+        <div style={{ 
+          width: "45%",
+          height: "90%",
+          minWidth: "600px",
+          minHeight: "300px",
+          background: "rgba(255,255,255,0.7)", 
+          borderRadius: "16px" }}>
+          <Line data={chartDataIntensity} options={chartOptionsIntensity} />
+        </div>
       </div>
       <div style={{
         width: "90%",
